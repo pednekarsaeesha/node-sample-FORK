71c379d013f274538b2b77555dc3c37ac0aa6f1f
nikhil <nikhilchowdhury@saarthiapp.com>
2022-07-13 11:39:27 +0530

    fixed logs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Ankit trying pm2 chal jaa')
+  res.send('Hello World! Ankit trying pm2 chal')
 })
 
 app.listen(port, () => {
