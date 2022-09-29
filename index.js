b47ca0e4bbe3f03744c33a4b7d9d46a4a8ac7d68
nikhil <nikhilchowdhury@saarthiapp.com>
2022-09-29 22:29:27 +0530

    logger

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const logger = require('./logger')
 
 app.get('/', (req, res) => {
+  logger.info("we have hit the get api")
   res.send('Hello World! Its a new day ')
 })
 
+app.get('/health', (req, res) => {
+  logger.info(JSON.stringify(req))
+  logger.info("Health of the app is fine")
+  res.send('Health of the app is fine')
+})
+
 app.listen(port, () => {
+  logger.debug('App is running')
   console.log(`Example app listening on port2 ${port}`)
 })
